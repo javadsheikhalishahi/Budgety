2f159c601647e48b1a9f564ff1fe30a0b36af88c
javadsheikhalishahi <140497195+javadsheikhalishahi@users.noreply.github.com>
2025-07-13 13:37:50 +0330

    summary endpoint: balance, income , expenses

--- backend/server.js
+++ backend/server.js
@@ -100,9 +100,23 @@ app.get("/api/transactions/summary/:userId", async(req,res) => {
     const {userId} = req.params;
 
     const balanceResult = await sql`
-      SELECT COALESCE(SUM(amount),0) as balance FROM transactions WHERE user_id = ${userId}
+      SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
     `
 
+const incomeResult = await sql`
+  SELECT COALESCE(SUM(amount), 0) as income FROM transactions
+  WHERE user_id = ${userId} AND amount > 0
+`
+const expensesResult = await sql`
+  SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions
+  WHERE user_id = ${userId} AND amount < 0
+`
+
+res.status(200).json({
+  balance: balanceResult[0].balance,
+  income: incomeResult[0].income,
+  expenses: expensesResult[0].expenses,
+})
   } catch (error) {
     console.log("Error getting the summary", error);
     res.status(500).json({message: "Internal server error"});
